b4c497e3726bd01f13dd9c27d05a2c4a20198575
999SH <94057132+999SH@users.noreply.github.com>
2023-04-18 10:05:47 +0200

    Swipe done

--- weather-expo/App.js
+++ weather-expo/App.js
@@ -1,6 +1,7 @@
 import * as React from "react";
-import { Button, View, Text } from 'react-native';
+import { Button, View, Text, useWindowDimensions } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import { TabView, SceneMap } from "react-native-tab-view";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 function HomeScreen({ navigation }) {
@@ -9,7 +10,7 @@ function HomeScreen({ navigation }) {
       <Text>Home Screen</Text>
       <Button
         title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
+        onPress={() => navigation.navigate("Details")}
       />
     </View>
   );
@@ -23,17 +24,52 @@ function DetailsScreen() {
   );
 }
 
+const FirstRoute = ({ navigation }) => (
+  <View style={{ flex: 1, backgroundColor: "#ff4081" }}>
+    <Button title="Go home" onPress={() => navigation.navigate("Home")} />
+  </View>
+);
+
+const SecondRoute = () => (
+  <View style={{ flex: 1, backgroundColor: "#673ab7" }} />
+);
+
+const renderScene = SceneMap({
+  first: FirstRoute,
+  second: SecondRoute,
+});
+
+function SwipeScreen() {
+  const layout = useWindowDimensions();
+
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: "first", title: "First" },
+    { key: "second", title: "Second" },
+  ]);
+
+  return (
+    <TabView
+      navigationState={{ index, routes }}
+      renderScene={renderScene}
+      onIndexChange={setIndex}
+      initialLayout={{ width: layout.width }}
+    />
+  );
+}
+
 const Stack = createNativeStackNavigator();
 
 <Stack.Screen
   name="Home"
   component={HomeScreen}
-  options={{ title: 'Overview' }}
-/>
+  options={{ title: "Overview" }}
+/>;
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="Swipe" component={SwipeScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
