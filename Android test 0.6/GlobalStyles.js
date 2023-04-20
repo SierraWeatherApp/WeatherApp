ef47a511762cbe1f0181c37b704fd745eea579f3
ChickenTheSlayer <87721901+ChickenTheSlayer@users.noreply.github.com>
2023-04-20 08:47:13 +0200

    Add files via upload

---
+++ Android test 0.6/GlobalStyles.js
@@ -0,0 +1,62 @@
+/* fonts */
+export const FontFamily = {
+  heading1Medium: "Montserrat_regular",
+  montserratBold: "Montserrat_bold",
+  alataRegular: "Alata_regular",
+  alegreyaSansMedium: "Alegreya Sans_medium",
+  alegreyaSansBold: "Alegreya Sans_bold",
+  interRegular: "Inter_regular",
+  alegreyaSansRegular: "Alegreya Sans_regular",
+  alegreyaSansThin: "Alegreya Sans_thin",
+  montserratLight: "Montserrat_light",
+  montserratSemibold: "Montserrat_semibold",
+  montserratAlternatesRegular: "Montserrat Alternates_regular",
+};
+/* font sizes */
+export const FontSize = {
+  heading1Medium_size: 24,
+  size_base: 16,
+  size_sm: 14,
+  size_xs: 12,
+  size_5xs_1: 7,
+  size_xl: 20,
+  size_6xs: 7,
+  size_7xs: 6,
+  size_2xs: 11,
+  size_5xs: 8,
+  size_mini: 15,
+  size_mid_6: 18,
+  size_4xs_1: 8,
+  size_35xl_3: 54,
+  size_4xs_2: 8,
+  size_mid_7: 18,
+  size_13xl: 32,
+};
+/* Colors */
+export const Color = {
+  white: "#fff",
+  black: "#000",
+  gainsboro_100: "#dedede",
+  gainsboro_200: "#d9d9d9",
+  red: "#ff0000",
+  lightskyblue: "#6fb1ee",
+  darkslategray: "#343844",
+  gray_100: "#888",
+  gray_200: "#332821",
+  gray_300: "rgba(255, 255, 255, 0)",
+  darkslateblue: "#18234f",
+  indianred: "#e95f67",
+};
+/* Paddings */
+export const Padding = {
+  p_xl: 20,
+};
+/* border radiuses */
+export const Border = {
+  br_4xl: 23,
+  br_4xs: 9,
+  br_5xs_1: 7,
+  br_7xs: 6,
+  br_2xs_9: 11,
+  br_mid: 17,
+};
---
+++ Android test 0.6/screens/RecommendationsFeedback.js
@@ -0,0 +1,142 @@
+import * as React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { Border, FontSize, FontFamily, Color } from "../GlobalStyles";
+
+const RecommendationsFeedback = () => {
+  return (
+    <View style={styles.recommendationsFeedback}>
+      <View style={[styles.editCity, styles.editLayout1]}>
+        <View style={[styles.editCityChild, styles.editPosition]} />
+        <Text style={[styles.feedback, styles.submitTypo]}>Feedback</Text>
+      </View>
+      <View style={[styles.editCity1, styles.editLayout]}>
+        <View style={[styles.editCityItem, styles.editLayout]} />
+        <Text style={[styles.submit, styles.submitTypo]}>Submit</Text>
+      </View>
+      <View
+        style={[
+          styles.recommendationsFeedbackChild,
+          styles.transferFramePosition,
+        ]}
+      />
+      <View style={[styles.transferFrame, styles.transferFramePosition]}>
+        <Text style={styles.pleaseLeaveYour}>
+          Please leave your feedback...
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  editLayout1: {
+    height: 32,
+    width: 319,
+    position: "absolute",
+  },
+  editPosition: {
+    borderRadius: Border.br_4xs,
+    left: 0,
+    top: 0,
+  },
+  submitTypo: {
+    textAlign: "center",
+    fontSize: FontSize.size_sm,
+    top: 7,
+    fontFamily: FontFamily.heading1Medium,
+    position: "absolute",
+  },
+  editLayout: {
+    height: 31,
+    width: 116,
+    position: "absolute",
+  },
+  transferFramePosition: {
+    top: 100,
+    position: "absolute",
+  },
+  editCityChild: {
+    backgroundColor: Color.white,
+    height: 32,
+    width: 319,
+    position: "absolute",
+  },
+  feedback: {
+    left: 55,
+    color: Color.black,
+    width: 208,
+    height: 18,
+  },
+  editCity: {
+    top: 28,
+    left: 21,
+    shadowOpacity: 1,
+    elevation: 4,
+    shadowRadius: 4,
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowColor: "rgba(0, 0, 0, 0.25)",
+  },
+  editCityItem: {
+    backgroundColor: Color.lightskyblue,
+    borderRadius: Border.br_4xs,
+    left: 0,
+    top: 0,
+  },
+  submit: {
+    left: 32,
+    color: Color.white,
+  },
+  editCity1: {
+    top: 631,
+    left: 226,
+    shadowOpacity: 1,
+    elevation: 4,
+    shadowRadius: 4,
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowColor: "rgba(0, 0, 0, 0.25)",
+  },
+  recommendationsFeedbackChild: {
+    left: 28,
+    width: 304,
+    height: 489,
+    backgroundColor: Color.white,
+  },
+  pleaseLeaveYour: {
+    top: 13,
+    right: -8,
+    fontSize: FontSize.size_xs,
+    color: Color.darkslategray,
+    textAlign: "left",
+    width: 305,
+    height: 34,
+    textShadowColor: "rgba(0, 0, 0, 0.25)",
+    textShadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    textShadowRadius: 4,
+    fontFamily: FontFamily.heading1Medium,
+    position: "absolute",
+  },
+  transferFrame: {
+    left: 26,
+    width: 306,
+    height: 50,
+    overflow: "hidden",
+  },
+  recommendationsFeedback: {
+    backgroundColor: Color.gainsboro_100,
+    flex: 1,
+    width: "100%",
+    height: 800,
+    overflow: "hidden",
+  },
+});
+
+export default RecommendationsFeedback;
