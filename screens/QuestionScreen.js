9e7e16f014f0d833878158ad822f05aaf42539c6
Tim <sonnenpelzxx@gmail.com>
2023-05-10 16:23:28 +0200

    bugfix

--- screens/QuestionScreen.js
+++ screens/QuestionScreen.js
@@ -1,16 +1,49 @@
 import React, { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { SafeAreaView, View, Text, TouchableOpacity, StyleSheet, ScrollView, Image, Pressable } from 'react-native';
 import { getIP, getDevice } from "./fetchIP"
 import Slider from '@react-native-community/slider';
-
-
+import { getWeather } from "../screens/CodeToWeather";
+import { setCities } from '../actions/cities';
 
 const QuestionScreen = ({ navigation }) => {
   const [questions, setQuestions] = useState([]);
   const [answers, setAnswers] = useState({});
   const deviceID = useSelector(state => state.deviceID)
 
+  const fetchCities = async () => {
+    const response = await fetch(
+      `http://${getIP()}:8080/api/v1/user?temperature=true&weathercode=true&windspeed=true&relativehumidity_2m=true`, {
+          method: 'GET',
+          headers: {'x-device-id': deviceID}
+        }
+    );
+    const jsonData = await response.json();
+    var cityArray = []
+    for(var i = 0; i < jsonData['cities'].length; i++){
+      weather = jsonData['cities'][i]['weather']
+      //weather = await fetchWeather(jsonData['cities'][i]["latitude"],jsonData['cities'][i]["longitude"] )
+      cityArray.push({city_name: jsonData['cities'][i]['city_name'], 
+        key: i + 1, 
+        country: jsonData['cities'][i]['country'],
+        id: jsonData['cities'][i]['id'], 
+        temperature: weather['temperature'], 
+        weather: getWeather(weather['weathercode']),
+        weathercode: weather['weathercode'],
+        humidity: weather['relativehumidity_2m'],
+        windspeed: weather['windspeed'],
+        lat: jsonData['cities'][i]['latitude'], 
+        long: jsonData['cities'][i]['longitude'], 
+        hat: jsonData['cities'][i]['recommendation'][0],
+        shirt: jsonData['cities'][i]['recommendation'][1],
+        jacket: jsonData['cities'][i]['recommendation'][2],
+        pants: jsonData['cities'][i]['recommendation'][3],
+        shoes: jsonData['cities'][i]['recommendation'][4],
+        umbrella: jsonData['cities'][i]['recommendation'][5],
+      })
+    }
+    dispatch(setCities(cityArray))
+  };
   useEffect(() => {
     const device_id = deviceID;
     fetch(`http://${getIP()}:8080/api/v1/user/questions/all`, {
@@ -67,6 +100,7 @@ const QuestionScreen = ({ navigation }) => {
         navigation.navigate('Home');
       })
       .catch(error => console.error(error));
+    fetchCities()
   };
 
   return (
